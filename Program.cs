fe54f0d1d263d4dce559c5b488b42452be672c9d
m.witzik <m.witzik@bde-engineering.de>
2023-01-15 22:19:37 +0100

    synch

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AoC_2022_Day03
 {
     class Program
     {
+        public const string FilePath = @"C:\ZZZ MW Daten\AoC_2022_Day03_Input_Original.txt";
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            var Input = File.ReadAllText(FilePath)
+            .Split("\r\n", StringSplitOptions.None)
+            .ToArray();
+
+            var Output = Input;
+
+
+
+
+            Console.WriteLine("Anwendung ist responsive");
+            Console.ReadKey();
         }
     }
 }
